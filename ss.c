12f8edcb8a360e6cc4a5845a4117e35e78260753
lexvny <1078307084@qq.com>
2023-03-21 20:59:13 +0800

    delete1

--- ss.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-#define size 100
-int main(){
-int a=1,b=a++;
-printf("%d %d",b,a);
-return 0;
-}
--- task/helloworld.java
+++
@@ -1,5 +0,0 @@
-package task;
-public class helloworld{
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-	}}
